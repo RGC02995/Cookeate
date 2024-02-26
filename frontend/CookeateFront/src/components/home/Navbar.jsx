21470cc68393546c77886f38bdd82aa2d98ea4a1
RGC02995 <raulgc2995@gmail.com>
2024-02-26 21:00:13 +0100

    create components Home and navbar and conf html

--- frontend/CookeateFront/src/components/home/Navbar.jsx
+++ frontend/CookeateFront/src/components/home/Navbar.jsx
@@ -1,9 +1,24 @@
-const Navbar =() => {
-    return (
-        <div>
-            soy el navbar
-        </div>
-    )
-}
+import Logo from "../../img/Logo.png";
+import { VscAccount } from "react-icons/vsc";
+import { DiAptana } from "react-icons/di";
 
-export default Navbar
+const Navbar = () => {
+  return (
+    <div className="nav_dimension">
+      <nav className="style_container">
+        <img className="img_dimension" src={Logo} alt="cookeate-logo.png" />
+        <div className="flex">
+          <label>
+            <input type="text" placeholder="Buscar..." />
+          </label>
+          <div className="icon_container">
+            <VscAccount className="icon_style" />
+            <DiAptana className="icon_style" />
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
